f815c26f29ba906e71b91cfcb001d5d23c54ffcf
Alex <ad222kr@student.lnu.se>
2015-10-23 21:22:54 +0200

    Heroku errors makes no sense, says it cant find BeerException when Im asking to require PubException. WHAT THE FUCK

--- src/app/controller/MasterController.php
+++ src/app/controller/MasterController.php
@@ -14,11 +14,6 @@ require_once("src/app/view/NavigationView.php");
 require_once("src/app/view/LayoutView.php");
 require_once("src/app/view/PubView.php");
 
-require_once("src/common/exceptions/PubAlreadyExistsException.php");
-
-
-
-
 //TODO: move exception requires to classes that use them.
 //TODO: try to implement autoloader if time allows.
 
